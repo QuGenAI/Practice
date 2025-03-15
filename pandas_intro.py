d26ebffe2c95e048f0eb1f326f49b7d71d13a9a0
QuGenAI <ramsinghkushinagar@yahoo.co.in>
2025-03-15 14:55:52 +0530

    added more code

--- pandas_intro.py
+++ pandas_intro.py
@@ -1,3 +1,5 @@
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/QuGenAI/Practice/master/data/iris.csv')
 df.head()
+df.tail()
+# Compare this snippet from github/command.py:
