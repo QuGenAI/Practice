ad3bb32270f3cd55e73b857ce468bca3bddd6514
QuGenAI <ramsinghkushinagar@yahoo.co.in>
2025-03-15 15:08:27 +0530

    adding code

--- pandas_intro.py
+++ pandas_intro.py
@@ -2,4 +2,7 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/QuGenAI/Practice/master/data/iris.csv')
 df.head()
 df.tail()
-# Compare this snippet from github/command.py:
+# Compare this snippet from github/command.py:  # git clone
+# git clone
+df.describe()
+df.info()
